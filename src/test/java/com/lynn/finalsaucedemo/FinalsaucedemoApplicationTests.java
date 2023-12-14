cbcb50ade818511f28f8b64b67084b1816786c2a
DavidOG <davidrogola@gmail.com>
2023-12-14 23:22:05 +0100

    Add sample test

--- src/test/java/com/lynn/finalsaucedemo/FinalsaucedemoApplicationTests.java
+++ src/test/java/com/lynn/finalsaucedemo/FinalsaucedemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.lynn.finalsaucedemo;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,11 @@ class FinalsaucedemoApplicationTests {
 
   @Test
   void contextLoads() {
+  }
+
+  @Test
+  void add_numbers() {
+    Assertions.assertThat(2 + 2).isEqualTo(4);
   }
 
 }
